b09c04c9dd2d75096ca692d9e1af8f7fefb146fa
Cristiano Souza <ltianosouzal@outlook.com>
2024-09-23 11:19:52 +0000

    Installing dependencies and configuring devise and rspec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
   allow_browser versions: :modern
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image_url])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image_url])
+  end
 end
