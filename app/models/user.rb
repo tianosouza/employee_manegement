aacd47461b53a3c0083f9af3d91076cb97516cd5
Cristiano Souza <ltianosouzal@outlook.com>
2024-09-23 11:46:45 +0000

    fix: not null image and add home_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_one_attached :image
+  
+  validates :name , presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }  
+  validates :password, presence: true, length: { minimum: 6 }
+
+  before_save :set_default_image
+
+  def first_name
+    name.split.first
+  end
+
+  def set_default_image
+    self.image_url ||= "default_image"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_23_113508) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_23_114412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_23_113508) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.string "image_url", null: false
+    t.string "image_url"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
